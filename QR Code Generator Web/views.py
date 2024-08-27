f73061160c7aeda7088a5c94c8bd78b1c44132eb
Ben Crittenden <130415312+BenCritt@users.noreply.github.com>
2024-08-27 16:03:03 -0500

    Update views.py

--- QR Code Generator Web/views.py
+++ QR Code Generator Web/views.py
@@ -1,36 +1,41 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from .forms import ReviewForm
-import os
-import qrcode
-from django.http import HttpResponse
-
+# This is the code for the QR Code Generator.
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def qr_code_generator(request):
+    # Check if the request method is POST.
     if request.method == "POST":
-        form = ReviewForm(request.POST)
+        # Initialize the form with data from the request.
+        form = QRForm(request.POST)
+        # Validate the form.
         if form.is_valid():
+            # Extract the data to be encoded in the QR code.
             data = form.cleaned_data["qr_text"]
+            # Initialize a QR Code generator.
             qr = qrcode.QRCode(version=1, box_size=10, border=5)
+            # Add the data to the QR Code.
             qr.add_data(data)
+            # Optimize the QR code layout.
             qr.make(fit=True)
+            # Create an image from the QR Code instance.
             img = qr.make_image(fill_color="black", back_color="white")
+            # Determine the directory to save the QR code image.
             home_dir = os.path.expanduser("~")
             save_dir = os.path.join(home_dir, "Downloads")
+            # Create the directory if it doesn't exist.
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
+            # Define the filename and full path for the QR code image.
             filename = "qrcode.png"
             full_path = os.path.join(save_dir, filename)
+            # Save the QR code image to the specified path.
             img.save(full_path)
 
+            # Serve the QR code image as a downloadable file in the response.
             with open(full_path, "rb") as f:
                 response = HttpResponse(f.read(), content_type="image/png")
                 response["Content-Disposition"] = 'attachment; filename="qrcode.png"'
                 return response
-            return render(
-                request,
-                "projects/qr_code_generator.html",
-                context={"form": form},
-            )
+    # Handle non-POST requests by initializing an empty form.
     else:
-        form = ReviewForm
+        form = QRForm
+    # Render the QR code generator page with the form.
     return render(request, "projects/qr_code_generator.html", context={"form": form})
